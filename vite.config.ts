55017719c7209b96067a92f9b0af32be4764d19d
XKain1117 <cebesser1117@hotmail.com>
2024-08-19 13:45:49 -0700

    fixed vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.tsx",
-    }),
-  ],
+  plugins: [react({
+    include: "**/*.tsx",
+  })],
+  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
 });
